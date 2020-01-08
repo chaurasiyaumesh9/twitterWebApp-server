8e081a469d77e1e841989059b1bf6bc1b4c03f95
unknown <uchaurasiya@girsoftwareservices.com>
2020-01-08 15:55:38 +0530

    using ravi's credential

--- config/auth.js
+++ config/auth.js
@@ -1,12 +1,12 @@
 // expose our config directly to our application using module.exports
 module.exports = {
     twitterAuth : {
-        consumer_key      : 'jy94kPWqRJYsJ9O7zV23zYE3A',
-        consumer_secret   : 'MgXdxjaWTxhHBleBhi67AXtmMTSf2X2d1lXJ60j7oHwb7B9LYM',
+        consumer_key      : '7xuUATGIXa3g343NQfL0xwfKO',
+        consumer_secret   : 'RoWrw4NAgj1eTzM1wR4Ee9gwmG0kQKC7HgBAi6U9pxa5SSreGZ',
         // consumerKey      : 'u5h3Mu4EVEOeatsJCdkAWb2ip',
         // consumerSecret   : 'tnRS7uqqV94EiyJOisxG9lnMYXOL5DzdysuhSsY7p69I6HVKGE',
         callbackURL      : "http://127.0.0.1:8010/auth/twitter/callback",
-        access_token        : "294469405-GcJQxDQewpowcNcHs64BipW1QgAZFjQwSfUpiiXg",
-        access_token_secret  : "8vtAblnhMNOOhKd0BIgDX2YvXYW5wGqDtSA5EsKT8zk6W"
+        access_token        : "880716583450161152-tV3u67QIBSfxrRCESRuvwxU9GnVYsKq",
+        access_token_secret  : "PMpKpqBG6tsyfCnZnbWNKbq6ckWoxgPl2fdrLKpQkCCeW"
     },
 };
